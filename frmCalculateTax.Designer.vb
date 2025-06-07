77e2cad2cc2e32974eb26dea5bc29d17d230a081
Sarah Greenberg <68124710+bellgreenb@users.noreply.github.com>
2025-06-07 14:58:18 -0400

    Initial commit

---
+++ frmCalculateTax.Designer.vb
@@ -0,0 +1,112 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmIncomeTaxCalculator
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.txtTaxableIncome = New System.Windows.Forms.TextBox()
+        Me.txtIncomeTaxOwed = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(55, 36)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(123, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Taxable income:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(55, 77)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(133, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Income tax owed:"
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(75, 118)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(106, 38)
+        Me.btnCalculate.TabIndex = 1
+        Me.btnCalculate.Text = "&Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnExit.Location = New System.Drawing.Point(197, 118)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(106, 38)
+        Me.btnExit.TabIndex = 2
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'txtTaxableIncome
+        '
+        Me.txtTaxableIncome.Location = New System.Drawing.Point(203, 36)
+        Me.txtTaxableIncome.Name = "txtTaxableIncome"
+        Me.txtTaxableIncome.Size = New System.Drawing.Size(100, 26)
+        Me.txtTaxableIncome.TabIndex = 0
+        '
+        'txtIncomeTaxOwed
+        '
+        Me.txtIncomeTaxOwed.Location = New System.Drawing.Point(203, 71)
+        Me.txtIncomeTaxOwed.Name = "txtIncomeTaxOwed"
+        Me.txtIncomeTaxOwed.Size = New System.Drawing.Size(100, 26)
+        Me.txtIncomeTaxOwed.TabIndex = 5
+        Me.txtIncomeTaxOwed.TabStop = False
+        '
+        'frmIncomeTaxCalculator
+        '
+        Me.AcceptButton = Me.btnCalculate
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnExit
+        Me.ClientSize = New System.Drawing.Size(389, 182)
+        Me.Controls.Add(Me.txtIncomeTaxOwed)
+        Me.Controls.Add(Me.txtTaxableIncome)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frmIncomeTaxCalculator"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Income Tax Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents txtTaxableIncome As TextBox
+    Friend WithEvents txtIncomeTaxOwed As TextBox
+End Class
---
+++ frmCalculateTax.vb
@@ -0,0 +1,42 @@
+﻿Public Class frmIncomeTaxCalculator
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim totalIncome As Integer = CInt(txtTaxableIncome.Text)
+        Dim incomeTax As Decimal = 0D
+
+        'If totalIncome >= 0 AndAlso totalIncome < 8700 Then
+        'incomeTax = totalIncome * 0.1D
+        'ElseIf totalIncome >= 8700 AndAlso totalIncome < 35350 Then
+        'incomeTax = 870 + (totalIncome - 8700) * 0.15D
+        'ElseIf totalIncome >= 35350 AndAlso totalIncome < 85650 Then
+        'incomeTax = 4867 + (totalIncome - 35350) * 0.25D
+        'ElseIf totalIncome >= 85650 AndAlso totalIncome < 178650 Then
+        'incomeTax = 17442 + (totalIncome - 85650) * 0.28D
+        'ElseIf totalIncome >= 178650 AndAlso totalIncome < 388350 Then
+        'incomeTax = 43482 + (totalIncome - 178650) * 0.33D
+        ' Else
+        'incomeTax = 112683 + (totalIncome - 388350) * 0.35D
+        'End If
+
+        Select Case totalIncome
+            Case 0 To 8700
+                incomeTax = totalIncome * 0.1D
+            Case 8700 To 35350
+                incomeTax = 870 + (totalIncome - 8700) * 0.15D
+            Case 35350 To 85650
+                incomeTax = 4867 + (totalIncome - 35350) * 0.25D
+            Case 85650 To 178650
+                incomeTax = 17442 + (totalIncome - 85650) * 0.28D
+            Case 178650 To 388350
+                incomeTax = 43482 + (totalIncome - 178650) * 0.33D
+            Case Else
+                incomeTax = 112683 + (totalIncome - 388350) * 0.35D
+        End Select
+        txtIncomeTaxOwed.Text = FormatCurrency(incomeTax)
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+End Class
